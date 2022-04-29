eae6343f0967d2ca9bfdddad7087c1591f76c195
aximum13 <aximum2@yandex.ru>
2022-04-29 14:40:58 +0300

    Version 1.0: Memo, Link and Task

--- link.rb
+++ link.rb
@@ -1,14 +1,38 @@
 class Link < Post
   def initialize
+    # Вызовем одноимённый метод (initialize) родителя (Post) методом super
     super
 
-    @url = ""
+    # А потом добавим то, что будет отличаться у ребёнка — поле @url
+    @url = ''
   end
+
   def read_from_console
+    # Мы полностью переопределяем метод read_from_console родителя Post
 
+    # Попросим у пользователя адрес ссылки
+    puts "Введите адрес ссылки"
+    @url = STDIN.gets.chomp
+
+    # И описание ссылки (одной строчки будет достаточно)
+    puts "Напишите пару слов о том, куда ведёт ссылка"
+    @text = STDIN.gets.chomp
   end
 
-  def to_strings
+  def save
+    # Метод save во многом повторяет метод родителя, но отличия существенны
 
+    file = File.new(file_path, "w:UTF-8")
+    time_string = @created_at.strftime("%Y.%m.%d, %H:%M")
+    file.puts(time_string + "\n\r")
+
+    # Помимо текста мы ещё сохраняем в файл адрес ссылки
+    file.puts(@url)
+    file.puts(@text)
+
+    file.close
+
+    # Напишем пользователю, что запись добавлена
+    puts "Ваша ссылка сохранена"
   end
 end
